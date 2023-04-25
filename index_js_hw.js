6ac29d5bc732e1efca33eb0999362cbd417471b0
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-04-24 20:23:07 -0400

    modified index.js file remove toc

--- index_js_hw.js
+++ index_js_hw.js
@@ -1,6 +1,12 @@
+// Require packages
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
 
+// Convert fs.writeFile into Promise version of same
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Questions for user 
 inquirer.prompt([
   {
     type: 'input',
@@ -10,88 +16,81 @@ inquirer.prompt([
   {
     type: 'input',
     name: 'description',
-    message: 'Please provide a description of your project:',
+    message: 'Please provide a brief description of your project:',
   },
   {
     type: 'input',
     name: 'installation',
-    message: 'Please provide installation instructions:',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
   },
   {
     type: 'input',
     name: 'usage',
-    message: 'Please provide usage information:',
-  },
-  {
-    type: 'input',
-    name: 'contributing',
-    message: 'Please provide contribution guidelines:',
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'Please provide test instructions:',
+    message: 'Provide instructions and examples for use.',
   },
   {
     type: 'list',
     name: 'license',
-    message: 'Please select a license:',
-    choices: ['MIT', 'GPLv3', 'Apache-2.0', 'None'],
+    message: 'Choose a license for your project:',
+    choices: [
+      'MIT',
+      'Apache 2.0',
+      'GPL 3.0',
+      'BSD 3',
+      'None',
+    ],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please describe how other developers can contribute to your project:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide examples of how to run tests for your application.',
   },
   {
     type: 'input',
     name: 'github',
-    message: 'Please provide your GitHub username:',
+    message: 'What is your GitHub username?',
   },
   {
     type: 'input',
     name: 'email',
-    message: 'Please provide your email address:',
+    message: 'What is your email address?',
   },
-]).then((answers) => {
-  const { title, description, installation, usage, contributing, tests, license, github, email } = answers;
-  const filename = 'README.md';
-
-  // Generate the README content
-  const content = `
-# ${title}
+])
+.then((answers) => {
+  // Generate README content
+  const readMeContent = `
+# ${answers.title}
 
 ## Description
-${description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [License](#license)
-- [Questions](#questions)
+${answers.description}
 
 ## Installation
-${installation}
+${answers.installation}
 
 ## Usage
-${usage}
-
-## Contributing
-${contributing}
-
-## Tests
-${tests}
+${answers.usage}
 
 ## License
-This project is licensed under the ${license} license.
+[![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)](https://opensource.org/licenses/${answers.license})
+This application is covered by the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
 
 ## Questions
-For questions about this project, please see my GitHub at [${github}](https://github.com/${github}), or email me at ${email}.
-  `;
+If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at https://github.com/${answers.github}.
+`;
 
-  // Write the content to the README.md file
-  fs.writeFile(filename, content, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`File '${filename}' successfully created!`);
-    }
-  });
-});
+  // Write README file
+  return writeFileAsync('README.md', readMeContent);
+})
+.then(() => console.log('Successfully generated README.md file'))
+.catch((err) => console.error(err));
